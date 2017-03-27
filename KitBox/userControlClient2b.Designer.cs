1cddd23565966362833108f6f5929b630a928dc6
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2017-03-27 11:41:19 +0200

    Added sign in page

---
+++ KitBox/userControlClient2b.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace KitBox
+{
+    partial class userControlClient2b
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.txtIdentifier = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(334, 184);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(117, 23);
+            this.btnValidate.TabIndex = 0;
+            this.btnValidate.Text = "Valider la commande";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // txtIdentifier
+            // 
+            this.txtIdentifier.Location = new System.Drawing.Point(397, 41);
+            this.txtIdentifier.Name = "txtIdentifier";
+            this.txtIdentifier.Size = new System.Drawing.Size(100, 20);
+            this.txtIdentifier.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(397, 76);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(82, 41);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(75, 23);
+            this.btnReturn.TabIndex = 3;
+            this.btnReturn.Text = "retour";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // userControlClient2b
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtIdentifier);
+            this.Controls.Add(this.btnValidate);
+            this.Name = "userControlClient2b";
+            this.Size = new System.Drawing.Size(1091, 611);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.TextBox txtIdentifier;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnReturn;
+    }
+}
